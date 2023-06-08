beb646b20f1c1fe83c36a43b66c5227863c752c4
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-06-08 13:41:11 +0300

    add footer and sidebar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
 
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../utils/routes";
+
+import LOGO from "../../images/logo.svg";
+import styles from '../../styles/Footer.module.css';
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <section className={styles.footer}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO} alt="Stuff"/>
+        </Link>
+      </div>
+      <div className={styles.rights}>
+        Developed by {" "}
+        <a
+          href="#"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Developer
+        </a>
+      </div>
+      <div className={styles.socials}>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg className="icon">
+            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+          </svg>
+        </a>
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg className="icon">
+            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+          </svg>
+        </a>
+        <a
+          href="https://youtube.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg className="icon">
+            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+          </svg>
+        </a>
+      </div>
+    </section>
   );
 };
 
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { NavLink } from "react-router-dom";
+import styles from '../../styles/Sidebar.module.css';
+
+const Sidebar = () => {
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`/categories/${1}`}>Link</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.footer}>
+        <a href="#" target="_blank" className={styles.link}>
+          Help
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          className={styles.link}
+          style={{ textDecoration: "underline"}}
+        >
+          Terms & Conditions
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
